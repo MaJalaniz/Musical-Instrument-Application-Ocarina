9c2a0d5f6e2e230351024ba6616a650c692cacc3
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-07-28 15:59:31 -0500

    spliced konami exercise code to listen for note sequences

--- js/InstrumentApplication.js
+++ js/InstrumentApplication.js
@@ -6,5 +6,32 @@ window.addEventListener('keydown', function(e) {
     audio.currentTime = 0; //rewind to start, so that the sound is not sketchy
     if (!audio) return; // return stops the function and returns the value at that given moment, assuming the key is registered.
     audio.play();
+
+    const pressed = [];
+    const secretCode = 'afddg';
+    window.addEventListener('keyup', (e) => {
+        //console.log(e.key);
+        pressed.push(e.key);
+        pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+        if (pressed.join('').includes(secretCode)) {
+            console.log('DING DING!');
+            audio.play();
+        }
+    });
+
 });
+/*
+const pressed = [];
+const secretCode = 'afddg';
+window.addEventListener('keyup', (e) => {
+    //console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join('').includes(secretCode)) {
+        console.log('DING DING!');
+        audio.play();
+    }
+    console.log(pressed);
+});
+*/
 
