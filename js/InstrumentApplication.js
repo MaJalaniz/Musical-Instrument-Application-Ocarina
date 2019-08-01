23311faef3f80021c6a8e203868d38e37e6ba6b1
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-08-01 17:37:15 -0500

    Nocturne of Shadows added to queue

--- js/InstrumentApplication.js
+++ js/InstrumentApplication.js
@@ -7,10 +7,11 @@ window.addEventListener('keydown', function(e) {
     if (!audio) return; // return stops the function and returns the value at that given moment, assuming the key is registered.
     audio.play();
 
+    //==========================Writing it out the long way===============================
 
     const pressed = [];
     const serenade = 'afddg';
-    const song1 = 'dafdaf';
+
     window.addEventListener('keyup', (e) => {
         //console.log(e.key);
         pressed.push(e.key); //pushes e.keys to pressed Array
@@ -22,21 +23,34 @@ window.addEventListener('keydown', function(e) {
         }
 
     });
-        const presseds = [];
+        const pressed1 = [];
         const song = 'dafdaf';
         window.addEventListener('keyup', (e) => {
             //console.log(e.key);
-            presseds.push(e.key); //pushes e.keys to pressed Array
-            presseds.splice(song.length - 1, presseds.length - song.length); //splits them apart
-            if (presseds.join('').includes(song)) { //checks if pressed Array contains serenade
+            pressed1.push(e.key); //pushes e.keys to pressed Array
+            pressed1.splice(song.length - 1, pressed1.length - song.length); //splits them apart
+            if (pressed1.join('').includes(song)) { //checks if pressed Array contains serenade
                 let audio = new Audio("sounds/43-Ocarina-Song-of-Time.mp3");//define a new audio
                 audio.play();
                 console.log('You played: Song of Time!')
             }
         });
 
+        const pressed2 = [];
+        const song2 = 'gddagdf';
+        window.addEventListener('keyup', (e) => {
+            //console.log(e.key);
+            pressed2.push(e.key); //pushes e.keys to pressed Array
+            pressed2.splice(song2.length - 1, pressed2.length - song2.length); //splits them apart
+            if (pressed2.join('').includes(song2)) { //checks if pressed Array contains serenade
+                let audio = new Audio("sounds/65-Nocturne-Of-Shadow.mp3");//define a new audio
+                audio.play();
+                console.log('You played: Nocturne of Shadow!')
+            }
+    });
 
-    //Introduce a Switch/Case for multiple songs dafdaf'
+
+    //Introduce a Switch/Case for multiple songs
 
 });
 
