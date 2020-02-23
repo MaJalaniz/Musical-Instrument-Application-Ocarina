36b0b1fc5d27c07e07a8ed4de04bf0ee7ee987d1
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-02-23 17:35:48 -0600

    A button is now on the staff and complete

--- js/InstrumentApplication.js
+++ js/InstrumentApplication.js
@@ -173,7 +173,7 @@ function aButton(){
         let a = document.createElement('div');
         
         let d = document.createTextNode('A');
-        a.setAttribute('class', 'up col-1');
+        a.setAttribute('class', 'up');
 
         let b = document.createElement('p');
         b.setAttribute('class', 'arrow-up');
